debefeba1af1141b894c25f52799f89f9428bb87
5Farron <fariasamuel2001@gmail.com>
2024-02-20 21:56:20 +0100

    config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { JeuxComponent } from './jeux/jeux.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'jeux', component: JeuxComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
